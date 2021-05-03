b7e65c25dd6b507fd1a8a972532fb30acb042fed
Evan Hamilton <evanphamilton@gmail.com>
2021-05-03 11:25:18 -0700

    Add locks, update api tests

--- live_api_tests.py
+++ live_api_tests.py
@@ -20,42 +20,46 @@ get_tester_metadata = os.path.join(BASEPATH, "tester", "1", "metadata")
 get_tester_data = os.path.join(BASEPATH, "tester", "1")
 get_tester = os.path.join(BASEPATH, "test")
 
-print(get_tester_data)
+
+# Test uploading all of the files for a single tester
+with open(path_to_file, 'rb') as f1:
+	with open(path_to_file1, 'rb') as f2:
+		with open(path_to_file2, 'rb') as f3:
+			with open(path_to_pics_json, 'rb') as f4:
+				files = [
+				    ('tester.json', f1),
+				    ('video.mp4', f2),
+				    ('video.json', f3),
+				    ('pics.json', f4)
+				]
+				for file in os.listdir(path_to_pic_files):
+					path_to_picture = os.path.join(path_to_pic_files, file)
+					open_pic_file = open(path_to_picture, "rb")
+					files.append(('pics', open_pic_file))
+
+				t1_start = perf_counter()
+				test_response = requests.post(upload_metadata, files=files)
+				t1_stop = perf_counter()
+				print("Elapsed time during upload in seconds:",
+                                        t1_stop-t1_start)
+				if test_response.status_code != 200:
+					print(test_response.text)
+
+
+
+# Test getting tester data
 res = requests.get(get_tester_data)
+assert res.status_code == 200
 data = res.json()
 print(data['result']['picture_paths'])
 
 
-
-print(get_tester_metadata)
+# Test getting tester metadata
 res = requests.get(get_tester_metadata)
-print(res)
+assert res.status_code == 200
+print(res.json())
+
+# Test getting test data
+res = requests.get(get_tests+"/11")
+assert res.status_code == 200
 print(res.text)
-
-res = requests.get(get_tests+"/1")
-print(res)
-print(res.text)
-
-
-# with open(path_to_file, 'rb') as f1:
-# 	with open(path_to_file1, 'rb') as f2:
-# 		with open(path_to_file2, 'rb') as f3:
-# 			with open(path_to_pics_json, 'rb') as f4:
-# 				files = [
-# 				    ('tester.json', f1),
-# 				    ('video.mp4', f2),
-# 				    ('video.json', f3),
-# 				    ('pics.json', f4)
-# 				]
-# 				for file in os.listdir(path_to_pic_files):
-# 					path_to_picture = os.path.join(path_to_pic_files, file)
-# 					open_pic_file = open(path_to_picture, "rb")
-# 					files.append(('pics', open_pic_file))
-
-# 				t1_start = perf_counter()
-# 				test_response = requests.post(upload_metadata, files=files)
-# 				t1_stop = perf_counter()
-# 				print("Elapsed time during upload in seconds:",
-#                                         t1_stop-t1_start)
-# 				if test_response.status_code != 200:
-# 					print(test_response.text)
--- oculid/app.py
+++ oculid/app.py
@@ -1,17 +1,19 @@
 import logging
 import sqlalchemy as alchemy
 
+from threading import Lock
+from os import getcwd
+
 from flask import Flask, flash, request, redirect, url_for, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask import jsonify
-
-from os import getcwd
 
 cwd = getcwd()
 
 UPLOAD_FOLDER = '%s/data_folder' % cwd
 EXPECTED_FILES = set(['tester.json', 'video.mp4', 'pics',
                     'video.json', 'pics.json'])
+db_lock = Lock()
 
 app = Flask(__name__)
 app.config['SESSION_TYPE'] = 'filesystem'
@@ -38,7 +40,9 @@ def tester(id):
     """
     logger("Tester called for id %s " % id)
     if request.method == 'GET':
-        return get_tester_data(id)
+        with db_lock:
+            data = get_tester_data(id)
+        return data
 
 
 @app.route('/tester/<id>/metadata', methods=['GET'])
@@ -49,7 +53,9 @@ def tester_metadata(id):
     """
     logger("Tester metadata called for id %s " % id)
     if request.method == 'GET':
-        return get_tester_metadata(id)
+        with db_lock:
+            data = get_tester_metadata(id)
+        return data
 
 
 @app.route('/test/<id>', methods=['GET'])
@@ -68,57 +74,58 @@ def test(id):
 def upload_file():
     logger("Upload called")
     if request.method == 'POST':
-        # Compare set of files and expected files
-        # prevent malicious files being parsed
-        if set(request.files) != EXPECTED_FILES:
-            # We may wish to use a more helpful error code in proudction
-            abort(418, "Uploaded files do not match expected files")
+        with db_lock:
+            # Compare set of files and expected files
+            # prevent malicious files being parsed
+            if set(request.files) != EXPECTED_FILES:
+                # We may wish to use a more helpful error code in proudction
+                abort(418, "Uploaded files do not match expected files")
 
-        # First parse tester.json to create tester and
-        # determine where to save subsequent data
-        res, data = read_and_save_tester_json(request.files['tester.json'])
-        if not res:
-            abort(400, data)
+            # First parse tester.json to create tester and
+            # determine where to save subsequent data
+            res, data = read_and_save_tester_json(request.files['tester.json'])
+            if not res:
+                abort(400, data)
 
-        tester_id, tester_folder = data
+            tester_id, tester_folder = data
 
-        # Parse video.json, then save video file,
-        # then set video path. Separated to make refactoring
-        # with saving to something like S3 in mind.
-        res, data = read_video_json_save_data(
-                        request.files['video.json'],
-                        tester_id)
-        if not res:
-            abort(400, data)
+            # Parse video.json, then save video file,
+            # then set video path. Separated to make refactoring
+            # with saving to something like S3 in mind.
+            res, data = read_video_json_save_data(
+                            request.files['video.json'],
+                            tester_id)
+            if not res:
+                abort(400, data)
 
-        video_id = data
-        res = save_video_set_path(
-                        request.files['video.mp4'],
-                        video_id,
-                        tester_folder)
-        if not res:
-            abort(400, "Unable to save video.mp4")
+            video_id = data
+            res = save_video_set_path(
+                            request.files['video.mp4'],
+                            video_id,
+                            tester_folder)
+            if not res:
+                abort(400, "Unable to save video.mp4")
 
 
-        # Parse pics.json, then save pic files,
-        # then set pic paths. Separated to make refactoring
-        # with saving to something like S3 in mind.
-        res, data = parse_pic_json_save_data(
-                        request.files['pics.json'],
-                        tester_id)
-        if not res:
-            abort(400, data)
+            # Parse pics.json, then save pic files,
+            # then set pic paths. Separated to make refactoring
+            # with saving to something like S3 in mind.
+            res, data = parse_pic_json_save_data(
+                            request.files['pics.json'],
+                            tester_id)
+            if not res:
+                abort(400, data)
 
-        # The rest of these functions are returning success codes,
-        # based off data validation. Would introduce something similar going
-        # forward for pictures. For now, assume the saving works or raises.
-        pics_list = request.files.getlist('pics')
-        save_pictures_set_pathes(pics_list, tester_id, tester_folder)
+            # The rest of these functions are returning success codes,
+            # based off data validation. Would introduce something similar going
+            # forward for pictures. For now, assume the saving works or raises.
+            pics_list = request.files.getlist('pics')
+            save_pictures_set_pathes(pics_list, tester_id, tester_folder)
 
-        # Check that the tester's pictures and video have paths set
-        # as proxy for successful data upload
-        tester = Tester.query.filter_by(id=tester_id).first()
-        if not tester.pictures_uploaded or not tester.video_uploaded:
-            abort(400, "Video or pictures not uploaded successfully")
-        else:
-            return "Success"
+            # Check that the tester's pictures and video have paths set
+            # as proxy for successful data upload
+            tester = Tester.query.filter_by(id=tester_id).first()
+            if not tester.pictures_uploaded or not tester.video_uploaded:
+                abort(400, "Video or pictures not uploaded successfully")
+            else:
+                return "Success"
